ab6aa8e1c4effa3aed29df25f8609f991f7af2a1
Heidi Hinkel <hhinkel15@gmail.com>
2023-02-26 09:43:24 -0500

    Basic Button setup

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
-def startTimer():
+def start_timer():
     pass
 
 
-def stopTimer():
+def stop_timer():
     pass
 
 
-def resetTimer():
+def reset_timer():
     pass
 
 
@@ -19,18 +19,18 @@ window.title("Storytelling Timer Light")
 window.resizable(width=False, height=False)
 window.geometry("800x600")
 
-top_label = ttk.Label(window, text="Time Left: 1:15:00",)
+topLabel = ttk.Label(window, text="Time Left: 1:15:00", )
 
-button_frame = ttk.Frame(master=window, relief=tk.RAISED)
-start_button = tk.Button(button_frame, text="Start", background="green", width=10, command=startTimer)
-stop_button = tk.Button(button_frame, text="Stop", background="red", width=10, command=stopTimer)
-reset_button = tk.Button(button_frame, text="Reset", background="orange", width=10, command= resetTimer)
+buttonFrame = ttk.Frame(master=window, relief=tk.RAISED)
+startButton = tk.Button(buttonFrame, text="Start", background="green", width=10, command=start_timer)
+stopButton = tk.Button(buttonFrame, text="Stop", background="red", width=10, command=stop_timer)
+resetButton = tk.Button(buttonFrame, text="Reset", background="orange", width=10, command=reset_timer)
 
-start_button.grid(row=0, column=0, sticky="e", padx=5, pady=5)
-stop_button.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-reset_button.grid(row=2, column=0, sticky="e", padx=5, pady=5)
+startButton.grid(row=0, column=0, sticky="e", padx=5, pady=5)
+stopButton.grid(row=1, column=0, sticky="e", padx=5, pady=5)
+resetButton.grid(row=2, column=0, sticky="e", padx=5, pady=5)
 
-top_label.place(relx=0.5, rely=0.0, anchor="n")
-button_frame.place(relx=0.12, rely=0.5, anchor="e")
+topLabel.place(relx=0.5, rely=0.0, anchor="n")
+buttonFrame.place(relx=0.12, rely=0.5, anchor="e")
 
 window.mainloop()
