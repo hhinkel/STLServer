4cac5b852439c4c0f73766d1e5744a70fc108b40
Heidi Hinkel <hhinkel15@gmail.com>
2023-02-26 21:07:35 -0500

    Breaking into functions

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ TIME_ALLOWED_STRING = "01:15:00"
 TIME_ALLOWED_SECONDS = 0
 
 
-def convert_to_seconds(timeString):
-    timeList = list(int(item) for item in timeString.split(":"))
-    return timeList[0] * 3600 + (timeList[1] * 60) + timeList[2]
+def convertToSeconds(timeString):
+    time_list = list(int(item) for item in timeString.split(":"))
+    return time_list[0] * 3600 + (time_list[1] * 60) + time_list[2]
 
 
-def convert_to_time(seconds):
+def convertToTime(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
@@ -22,22 +22,22 @@ def convert_to_time(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
-def timer(function, timeInSeconds):
+def timer(function, time_in_seconds):
     if function and function != "Stop" and function != "Reset":
-        timeLeft = timeInSeconds
-        while timeLeft > 0 and (function != "Stop" or function != "Reset"):
-            print(convert_to_time(timeLeft))
+        time_left = time_in_seconds
+        while time_left > 0 and (function != "Stop" or function != "Reset"):
+            print(convertToTime(time_left))
             time.sleep(1)
-            timeLeft -= 1
+            time_left -= 1
 
 
-def start_timer():
-    timeSeconds = convert_to_seconds(TIME_ALLOWED_STRING)
-    timer("Start", timeSeconds)
+def startTimer():
+    time_seconds = convertToSeconds(TIME_ALLOWED_STRING)
+    timer("Start", time_seconds)
     print("Start button pressed")
 
 
-def stop_timer():
+def stopTimer():
     print("Stop button pressed")
 
 
@@ -45,23 +45,39 @@ def reset_timer():
     print("Reset button pressed")
 
 
-window = tk.Tk()
-window.title("Storytelling Timer Light")
-window.resizable(width=False, height=False)
-window.geometry("800x600")
+def interfaceButtons(window):
+    button_frame = ttk.Frame(master=window, relief=tk.RAISED)
+    start_button = tk.Button(button_frame, text="Start", background="green", width=10, command=startTimer)
+    stop_button = tk.Button(button_frame, text="Stop", background="red", width=10, command=stopTimer)
+    reset_button = tk.Button(button_frame, text="Reset", background="orange", width=10, command=reset_timer)
 
-topLabel = ttk.Label(window, text="Time Allowed: 1:15:00", )
+    start_button.grid(row=0, column=0, sticky="e", padx=5, pady=5)
+    stop_button.grid(row=1, column=0, sticky="e", padx=5, pady=5)
+    reset_button.grid(row=2, column=0, sticky="e", padx=5, pady=5)
 
-buttonFrame = ttk.Frame(master=window, relief=tk.RAISED)
-startButton = tk.Button(buttonFrame, text="Start", background="green", width=10, command=start_timer)
-stopButton = tk.Button(buttonFrame, text="Stop", background="red", width=10, command=stop_timer)
-resetButton = tk.Button(buttonFrame, text="Reset", background="orange", width=10, command=reset_timer)
+    button_frame.place(relx=0.12, rely=0.5, anchor="e")
 
-startButton.grid(row=0, column=0, sticky="e", padx=5, pady=5)
-stopButton.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-resetButton.grid(row=2, column=0, sticky="e", padx=5, pady=5)
 
-topLabel.place(relx=0.5, rely=0.0, anchor="n")
-buttonFrame.place(relx=0.12, rely=0.5, anchor="e")
+def interfaceClockDisplay(window):
+    clock = tk.Label(window, text=TIME_ALLOWED_STRING, font=("Courier", 20), width = 10)
+    clock.place(relx=0.5, rely=0.5, anchor="center")
 
-window.mainloop()
+
+def interface(window):
+    top_label = ttk.Label(window, text="Time Allowed: 1:15:00", )
+    interfaceButtons(window)
+    interfaceClockDisplay(window)
+    top_label.place(relx=0.5, rely=0.0, anchor="n")
+
+
+def main():
+    window = tk.Tk()
+    window.title("Timing Light")
+    window.resizable(width=False, height=False)
+    window.geometry("800x600")
+    interface(window)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
